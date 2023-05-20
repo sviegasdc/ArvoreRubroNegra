57ba72229fe2547ca7f1e00fc80767cc7ba4febc
S <77491046+sviegasdc@users.noreply.github.com>
2023-05-20 11:05:38 -0300

    finalizado(por enquanto),casos de teste criados e passaram, ficar de olho em mais erros

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,95 @@
 public class Main {
-    public static void main(String[] args) throws InvalidNoException {
+    public static void InsercaoRemocaoSimples() throws InvalidNoException {
         ArvoreRN rn = new ArvoreRN(10);
-        No a = rn.addChave(8);
-        No b = rn.addChave(1);
-        No c = rn.addChave(2);
-        No d = rn.addChave(4);
-        No e = rn.addChave(7);
-        No f = rn.addChave(5);
-        No g = rn.addChave(13);
-        No h = rn.addChave(16);
+        //TESTANDO CASOS INSERÇÃO
+        No a = rn.addChave(5);
+        No b = rn.addChave(15);
+        // fica bugado em temas claros
         rn.mostrarArvoreComCor();
         System.out.println("-----------------------------------------------------------------------------------------");
-        rn.removeChave(1);
-        // fica bugado em temas claros
+        No c = rn.addChave(3);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        No d = rn.addChave(7);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        No e = rn.addChave(12);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        No f = rn.addChave(17);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+        //TESTANDO CASOS REMOÇÃO
+        System.out.println("REMOÇÕES:");
+        rn.removeChave(17);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        rn.removeChave(12);
         rn.mostrarArvoreComCor();
         System.out.println("-----------------------------------------------------------------------------------------");
         rn.removeChave(7);
         rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        rn.removeChave(3);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        rn.removeChave(15);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        rn.removeChave(5);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        rn.removeChave(10);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+    }
 
+    public static void InsercaoRemocaoComplexa() throws InvalidNoException{
+        ArvoreRN rn = new ArvoreRN(10);
+        //TESTANDO CASOS INSERÇÃO
+        No a = rn.addChave(8);
+        No b = rn.addChave(1);
+        // fica bugado em temas claros
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        No c = rn.addChave(2);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        No d = rn.addChave(4);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        No e = rn.addChave(7);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        No f = rn.addChave(5);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        No g = rn.addChave(13);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        No h = rn.addChave(16);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+        //TESTANDO CASOS REMOÇÃO
+        System.out.println("REMOÇÕES:");
+        rn.removeChave(1);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        rn.removeChave(7);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        rn.removeChave(10);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        rn.removeChave(5);
+        rn.mostrarArvoreComCor();
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+    }
+    public static void main(String[] args) throws InvalidNoException {
+        //InsercaoRemocaoSimples();
+        //InsercaoRemocaoComplexa();
     }
 }
